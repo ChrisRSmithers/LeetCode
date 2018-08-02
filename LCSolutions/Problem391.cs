44aeb24a17d44135d53554212df5d67916e7655b
ChrisRSmithers <c.smithers@hotmail.co.uk>
2018-08-02 13:33:57 +0100

    Tests added and Stub solve method
    Some tests pass since the stub always returns false. There is currently no implementation

---
+++ LCSolutions/Problem391.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    public static class Problem391
+    {
+        public static bool Solve(int[,] Rectangles)
+        {
+            //Stub
+            return false;
+        }
+    }
+}
