d97b97da1240a62ff870468eb6619c388947e5d6
ChrisRSmithers <c.smithers@hotmail.co.uk>
2018-08-03 16:08:12 +0100

    Stub solution and Unit Tests
    Not many tests, hard to think of any concrete examples

---
+++ LCSolutions/Problem149.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public Point() { x = 0; y = 0; }
+        public Point(int a, int b) { x = a; y = b; }
+    }
+
+    public static class Problem149
+    {
+        public static int MaxPoints(Point[] points)
+        {
+            return 0;
+        }
+    }
+}
