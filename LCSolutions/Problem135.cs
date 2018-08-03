d788d972fca9929306563db2f129d59ec37fd348
ChrisRSmithers <c.smithers@hotmail.co.uk>
2018-08-03 10:39:18 +0100

    Solution Complete
    All tests pass. O(N) in time and memory. Time is optimal complexity but memory could be improved

--- LCSolutions/Problem135.cs
+++ LCSolutions/Problem135.cs
@@ -8,7 +8,58 @@ namespace LeetCodeSolutions
     {
         static public int Candy(int[] ratings)
         {
-            return 0;
+            int numChildren = ratings.Length;
+            if (numChildren == 1)
+            {
+                return 1;
+            }
+
+            //Two pass algorithm, once from left and once from the right
+            //O(n) in time and memory
+
+            int[] CandiesLeft = new int[numChildren];
+            int prevRating = ratings[0];
+            CandiesLeft[0] = 1;
+
+            for (int ii = 1; ii < numChildren; ii++)
+            {
+                int currentRating = ratings[ii];
+                if (currentRating > prevRating)
+                {
+                    CandiesLeft[ii] = CandiesLeft[ii - 1] + 1;
+                }
+                else
+                {
+                    CandiesLeft[ii] = 1;
+                }
+                prevRating = currentRating;
+            }
+
+            prevRating = ratings[numChildren-1];
+            int[] CandiesRight = new int[numChildren];
+            CandiesRight[numChildren-1] = 1;
+
+            for (int ii = numChildren - 2; ii >= 0; ii--)
+            {
+                int currentRating = ratings[ii];
+                if (currentRating > prevRating)
+                {
+                    CandiesRight[ii] = CandiesRight[ii + 1] + 1;
+                }
+                else
+                {
+                    CandiesRight[ii] = 1;
+                }
+                prevRating = currentRating;
+            }
+
+            int answer = 0;
+            for (int ii = 0; ii < numChildren; ii++)
+            {
+                answer += Math.Max(CandiesLeft[ii], CandiesRight[ii]);
+            }
+
+            return answer;
         }
     }
 }
