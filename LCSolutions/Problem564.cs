e04b59363238fbdc8cf6f6d20276e8a38f9894b1
ChrisRSmithers <c.smithers@hotmail.co.uk>
2018-08-03 11:34:55 +0100

    Stub Solution with Unit Tests

---
+++ LCSolutions/Problem564.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    public static class Problem564
+    {
+        public static string NearestPalindromic(string n)
+        {
+            return "";
+        }
+    }
+}
